4876dc9bf075ec0a0b15d04de2613616dc7ffcd0
Air1K <veremeenkom91@gmail.com>
2022-08-13 19:58:25 +0500

    skroll_fix

--- src/jsx_post/Home/achiev/Achive_blok.jsx
+++ src/jsx_post/Home/achiev/Achive_blok.jsx
@@ -76,8 +76,16 @@ const Achive_blok = () => {
 
 
     const { scrollY } = useViewportScroll();
-    const y1 = useTransform(scrollY, [1000, 2200], [250, -500]);
+    const y1 = useTransform(scrollY, [2450, 3800], [250, -500]);
+    const y2 = useTransform(scrollY, [2850, 3900], [150, -500]);
+    const y3 = useTransform(scrollY, [3250, 4300], [250, -500]);
+    const y4 = useTransform(scrollY, [3850, 4900], [350, -100]);
 
+
+    const z1 = useTransform(scrollY, [2000, 3200], [150, -200]);
+    const z2 = useTransform(scrollY, [2000, 3200], [250, -500]);
+    const z3 = useTransform(scrollY, [2000, 3200], [350, -300]);
+    const z4 = useTransform(scrollY, [2000, 3200], [150, -500]);
     return (
         <div>
             <div className="begstrok">
@@ -88,36 +96,36 @@ const Achive_blok = () => {
             <h1 className="dostig-h1">Доска наших достижений</h1>
             <div className="top-tt-box">
                 <div className="collum-top-tt1">
-                    <div className="content-top-tt-koll1-el1">
+                    <motion.div style={{ y: y1 }} className="content-top-tt-koll1-el1">
                         <video autoPlay muted preload loop>
                             <source src={require("../../../video/8mill.mp4")} type="video/mp4"/>
                         </video>
                         <div className="fon-content-tt1_1"></div>
                         <div className="ramka"></div>
-                    </div>
-                    <div className="content-top-tt-koll1-el2">
+                    </motion.div>
+                    <motion.div style={{ y: y2 }} className="content-top-tt-koll1-el2">
                         <video autoPlay muted preload loop>
                             <source src={require("../../../video/3_1mill.mp4")} type="video/mp4"/>
                         </video>
                         <div className="fon-content-tt1_2"></div>
                         <div className="ramka"></div>
-                    </div>
+                    </motion.div>
                 </div>
                 <div className="collum-top-tt2">
-                    <div className="content-top-tt-koll2-el1">
+                    <motion.div style={{ y: y3 }} className="content-top-tt-koll2-el1">
                         <video autoPlay muted preload loop>
                             <source src={require("../../../video/1_1mill.mp4")} type="video/mp4"/>
                         </video>
                         <div className="fon-content-tt2_1"></div>
                         <div className="ramka"></div>
-                    </div>
-                    <div className="content-top-tt-koll2-el2" style={{ marginBottom: 0}}>
+                    </motion.div>
+                    <motion.div style={{ y: y4, marginBottom: 0}} className="content-top-tt-koll2-el2">
                         <video autoPlay muted preload loop>
                             <source src={require("../../../video/700k.mp4")} type="video/mp4"/>
                         </video>
                         <div className="fon-content-tt2_2"></div>
                         <div className="ramka"></div>
-                    </div>
+                    </motion.div>
                 </div>
                 <div className="fon_mem_stonks"></div>
             </div>
