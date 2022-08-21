45de68f92df728489e70cb90037c5a69485af3ca
Air1K <veremeenkom91@gmail.com>
2022-08-21 14:27:07 +0500

    register_end

--- src/jsx_post/authorization/sign_in/SignIn.jsx
+++ src/jsx_post/authorization/sign_in/SignIn.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import './style.sass'
 
 const SignIn = () => {
     return (
         <div>
-             <div className="autoriz-item">Register</div>
+             <div className="autoriz-item">Sign Up</div>
             <div className="logo_dov">
                 <img className="Logo_fon" src={require("../../../img-2/NO-NLfon.png")} alt="" />
                 <img className="Logo_bac" src={require("../../../img-2/NO-NLbac.png")} alt="" />
             </div>
+            <div className="input_full_name_div">
+                <input className='input1' autocomplete="off" type="text" name="name" id="name" placeholder="Name..." />
+            </div>
             <div className="input_log_div">
-                <input className='input1' autocomplete="off" type="text" name="email" id="email" placeholder="Email" />
+                <input className='input2' autocomplete="off" type="text" name="email" id="email" placeholder="Email" />
+            </div>
+            <div className="input_user_name_div">
+                <input className='input3' autocomplete="off" type="text" name="username" id="username" placeholder="Username..." />
             </div>
             <div className="input_pass_div">
-                <input className='input2' autocomplete="off" type="Password" name="password" id="password" placeholder="Password" />
+                <input className='input4' autocomplete="off" type="Password" name="password" id="password" placeholder="Password" />
+            </div>
+            <div className="input_repeat_password_div">
+                <input className='input4' autocomplete="off" type="Password" name="repeat_password" id="repeat_password" placeholder="Repeat Password" />
             </div>
             <div className="conteiner_Log_reg">
                 <div className="buttom_div">
