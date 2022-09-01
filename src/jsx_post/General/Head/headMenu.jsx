39ff0a056aced2d1129de97cd8483ac6ae1abd08
Air1K <veremeenkom91@gmail.com>
2022-09-01 11:41:31 +0500

    menu_user_head

--- src/jsx_post/General/Head/headMenu.jsx
+++ src/jsx_post/General/Head/headMenu.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { faSignIn } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, {useState} from 'react';
+import {faSignIn} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Link} from "react-router-dom";
+import './styleHead.sass'
 
-const headMenu = () => {
+const HeadMenu = () => {
+    const [menu_user, setMenuuser] = useState(false);
     return (
         <div className="content ">
             <div className="contents">
@@ -19,17 +21,38 @@ const headMenu = () => {
                             Вкл / вкл курсор
                             <div className="switch-btn switch-on"></div>
                         </div>
-                        
+
                         <img src={require("../../../img-2/head2.png")} alt=""></img>
                     </div>
-                    <div className="enter"><Link to="login"><FontAwesomeIcon icon={faSignIn} /><div className="enter-item">
-                        Войти
-                    </div></Link></div>
+                    <div className="enter">
+                        {/*<Link to="login">*/}
+                        {/*    <FontAwesomeIcon icon={faSignIn}/>*/}
+                        {/*    <div className="enter-item">*/}
+                        {/*        Войти*/}
+                        {/*    </div>*/}
+                        {/*</Link>*/}
+                        <div className="user">
+                            <div className="menu-user">
+                                <button
+                                    onClick={()=>{setMenuuser(true); console.log(menu_user)}}
+                                    onBlur={()=>{setMenuuser(false); console.log(menu_user)}} className="avarat_and_button">
+                                    <img className="avatar" src={require("../../../img-2/avatar_user/user5.jpg")}
+                                         alt=""/>
+                                    <img className="border-avatar" src={require("../../../img-2/border_avarar.png")}
+                                         alt=""/>
+                                </button>
+                                {menu_user ? (
+                                <div className="menu-user-drop">
+                                    dfgdgf
+                                </div>) : (<div/>)}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-                
+            </div>
+
         </div>
     );
 };
 
-export default headMenu;
+export default HeadMenu;
