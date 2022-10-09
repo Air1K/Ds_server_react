c6e49ede14c6f13d24ef0f1c087c22ecdfb68c55
Air1K <veremeenkom91@gmail.com>
2022-10-09 14:01:15 +0500

    Делаю адаптив menu

--- src/jsx_post/General/Head/MobileMenu/mobileMenu.tsx
+++ src/jsx_post/General/Head/MobileMenu/mobileMenu.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import styleMenu from './styleMenu.module.sass'
-const MobileMenu = ({setIs}) => {
+const MobileMenu = ({is, setIs}) => {
+    const rootClasses = [];//    const rootClasses = [];//rootClasses.join(' ')
+
+    console.log(is)
+    if(is){
+
+        rootClasses.push(styleMenu.content__menuOnClick)
+    }
+
+    //
 
     return (
         <div
@@ -9,7 +18,9 @@ const MobileMenu = ({setIs}) => {
         >
             <div
                 onClick={(e)=>{e.stopPropagation()}}
-                className={styleMenu.content__menu}>
+                className={styleMenu.content__menu}
+                // @ts-ignore
+                id={rootClasses}>
                 <div className={styleMenu.header}>No osu! - No life!</div>
             </div>
         </div>
