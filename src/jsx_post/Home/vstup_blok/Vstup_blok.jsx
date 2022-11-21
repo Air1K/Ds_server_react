5dc7d7829d533db004e979b31f6ad9d559ec17b7
Air1K <veremeenkom91@gmail.com>
2022-11-21 17:08:29 +0500

    Адаптив кнопки

--- src/jsx_post/Home/vstup_blok/Vstup_blok.jsx
+++ src/jsx_post/Home/vstup_blok/Vstup_blok.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus} from "@fortawesome/free-solid-svg-icons";
-
+import styles from './styleVstup_blok.module.sass'
 const Vstup_blok = () => {
     return (
         <div className="vstup_block">
-            <div className="sekt_inf" style={{marginTop: '30px'}}>
-                <div className="text-info">
+            <div className="sekt_inf" style={{marginTop: '20px'}}>
+                <div className={`text-info ${styles.text_info}`}>
                     Если тебя заинтересовал наш сервер и ты хочешь в нашу команду, тебе необходимо знать и
                     понимать как пользоваться ботами, интеграцией и многим другим на сервере. Если ты можешь и
                     хочешь модерировать сервер, приглашаем к нам в набор.
                 </div>
-                <a className="bottom_add_admin" href='https://discord.gg/xcrfrJ5QsN'>
-                    Вступить в команду <FontAwesomeIcon icon={faPlus} />
+                <a className={`bottom_add_admin ${styles.bottom_add_admin}`} href='https://discord.gg/xcrfrJ5QsN'>
+                    <div className={styles.bottom_1}>Вступить в команду</div > <div className={styles.bottom_2}><FontAwesomeIcon style={{fontSize: '20px'}} icon={faPlus} /></div>
                 </a>
             </div>
         </div>
